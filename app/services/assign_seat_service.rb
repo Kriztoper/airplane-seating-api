af5b01ef3f02990282e342d404665cf122791448
Kriztoper Urmeneta <kriztoper.urmeneta.pro@gmail.com>
2022-09-23 22:42:24 +0800

    (service) add is_aisle_seat checker method

--- app/services/assign_seat_service.rb
+++ app/services/assign_seat_service.rb
@@ -11,6 +11,35 @@ class AssignSeatService
     # end
   end
 
+  def is_aisle_seat(col_index, current_group, group_number)
+    last_col_index = current_group[0].size - 1
+
+    # if is aisle seat of 1st group
+    if group_number == 1
+      if col_index == last_col_index
+        return true
+      else
+        return false
+      end
+    end
+
+    # if is aisle seat of last group
+    if group_number == @group_size
+      if col_index == 0
+        return true
+      else
+        return false
+      end
+    end
+
+    # if is aisle seat of middle groups
+    if col_index == 0 or col_index == current_group[0].size - 1
+      return true
+    end
+
+    false
+  end
+
   def init_seats(dimensions)
     seats = []
     dimensions.each do |d|

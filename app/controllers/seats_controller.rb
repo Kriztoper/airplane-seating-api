404d5b45c209137a9044fa0536f1fe2f1db89f18
Kriztoper Urmeneta <kriztoper.urmeneta.pro@gmail.com>
2022-09-24 14:28:55 +0800

    (feature,api-endpoint) add assign seats api endpoint that accepts as body params the dimensions and num passengers then returns the 2D array of the seats assigned with passengers

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -5,4 +5,14 @@ class SeatsController < ApplicationController
     render json: seats, except: [:created_at, :updated_at]
   end
 
+  def assign_seats
+    num_passengers = params[:num_passengers].to_i
+    seats_dimensions = eval(params[:seats_dimensions].to_s)
+    service = AssignSeatService.new
+    assigned_seats = service.assign seats_dimensions, num_passengers
+    print "\nOutput for API:"
+    Array2dUtils.array_2D_print assigned_seats
+    render json: assigned_seats
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :airplanes
   resources :seats
+  post 'seats/assign-to-passengers', to: 'seats#assign_seats'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

a20222e0394c19a7056113d5d02c1d4cb78d6d24
Kriztoper Urmeneta <kriztoper.urmeneta.pro@gmail.com>
2022-09-24 21:23:46 +0800

    (refactor) use shorthand boolean return and extract to variable boolean conditions

--- app/services/assign_seat_service.rb
+++ app/services/assign_seat_service.rb
@@ -3,8 +3,8 @@ class AssignSeatService
   def assign(dimensions, num_passengers)
     seats = init_seats dimensions
 
-    @group_size = seats.size
-    puts "group size: #{@group_size}"
+    group_size = seats.size
+    puts "group size: #{group_size}"
     @max_row_index = 0
 
     passenger_count = 1
@@ -25,12 +25,12 @@ class AssignSeatService
         row.each_with_index do |seat, col_index|
 
           puts "row_index: #{row_index}, col_index: #{col_index}"
-          puts "is_aisle_seat: #{is_aisle_seat(col_index, group, group_number)}"
-          puts "is_window_seat: #{is_window_seat(col_index, group, group_number)}"
-          if is_aisle_seat(col_index, group, group_number)
+          puts "is_aisle_seat: #{is_aisle_seat(col_index, group, group_number, group_size)}"
+          puts "is_window_seat: #{is_window_seat(col_index, group, group_number, group_size)}"
+          if is_aisle_seat(col_index, group, group_number, group_size)
             seat = Seat.new group: group_number, x: row_index, y: col_index, airplane_id: 1
             aisle_seats.append seat
-          elsif is_window_seat(col_index, group, group_number)
+          elsif is_window_seat(col_index, group, group_number, group_size)
             seat = Seat.new group: group_number, x: row_index, y: col_index, airplane_id: 1
             window_seats.append seat
           else
@@ -47,54 +47,48 @@ class AssignSeatService
     assign_passengers_to_all_grouped_seats(aisle_seats, center_seats, num_passengers, seats, window_seats)
   end
 
-  def is_aisle_seat(col_index, current_group, group_number)
+  def is_aisle_seat(col_index, current_group, group_number, group_size)
     last_col_index = current_group[0].size - 1
 
+    is_first_column = col_index == 0
+    is_last_column = col_index == last_col_index
+
+    is_first_group = group_number == 1
+    is_last_group = group_number == group_size
+
     # if is aisle seat of 1st group
-    if group_number == 1
-      if col_index == last_col_index
-        return true
-      else
-        return false
-      end
+    if is_first_group
+      return is_last_column
     end
 
     # if is aisle seat of last group
-    if group_number == @group_size
-      if col_index == 0
-        return true
-      else
-        return false
-      end
+    if is_last_group
+      return is_first_column
     end
 
     # if is aisle seat of middle groups
-    if col_index == 0 or col_index == current_group[0].size - 1
+    if is_first_column or is_last_column
       return true
     end
 
     false
   end
 
-  def is_window_seat(col_index, current_group, group_number)
+  def is_window_seat(col_index, current_group, group_number, group_size)
     last_col_index = current_group[0].size - 1
 
     # if is window seat of 1st group
-    if group_number == 1
-      if col_index == 0
-        return true
-      else
-        return false
-      end
+    is_first_group = group_number == 1
+    if is_first_group
+      is_first_column = col_index == 0
+      return is_first_column
     end
 
     # if is window seat of last group
-    if group_number == @group_size
-      if col_index == last_col_index
-        return true
-      else
-        return false
-      end
+    is_last_group = group_number == group_size
+    if is_last_group
+      is_last_column = col_index == last_col_index
+      return is_last_column
     end
 
     false
